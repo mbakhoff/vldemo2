c29bb3abd1daab32ec78a92cd0e9b5ba56450054
Märt Bakhoff <mart.bakhoff@gmail.com>
2014-03-07 14:37:04 +0200

    listing articles

--- src/main/webapp/assets/demoapp.js
+++ src/main/webapp/assets/demoapp.js
@@ -3,14 +3,20 @@ $(function() {
     console.log("running demoapp.js");
 
     function loadArticles(list, articles) {
-        console.log(articles);
+        // http://api.jquery.com/jQuery.each/
+        $.each(articles, function(i, article) {
+            var newListItem = $('<li/>');
+            newListItem.text(article.title);
+            list.append(newListItem);
+        });
     }
 
+    // http://api.jquery.com/jQuery.ajax/
     $.ajax('/newspaper', {
         dataType: 'json',
         success: function(articlesJson) {
             console.info('ajax success');
-            var articlesList = $('articles');
+            var articlesList = $('#articles');
             loadArticles(articlesList, articlesJson);
         },
         error: function(req, text, error) {
